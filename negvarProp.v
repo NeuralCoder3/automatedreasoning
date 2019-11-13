91a8845299dd43cd3b081e243ae79b865899f186
Marcel Ullrich <s8maullr@stud.uni-saarland.de>
2019-11-13 23:17:47 +0100

    moved negvar

--- negvarProp.v
+++ negvarProp.v
@@ -1,5 +1,17 @@
 Load properties.
 
+Fixpoint negvar {Pi} (F:@formula Pi) :=
+  match F with
+    V p => Not (V p)
+  | Not f => Not (negvar f)
+  | And f1 f2 => And (negvar f1) (negvar f2)
+  | Or f1 f2 => Or (negvar f1) (negvar f2)
+  | To f1 f2 => To (negvar f1) (negvar f2)
+  | Equiv f1 f2 => Equiv (negvar f1) (negvar f2)
+  | _ => F
+  end.
+
+
 Lemma negvarEval: forall X (F:@formula X) A, eval A F = eval (fun a => negb (A a)) (negvar F).
 Proof.
   intros Pi F A.
@@ -13,6 +25,7 @@ Goal forall X (F:@formula X), satisfiable F -> satisfiable (negvar F).
 Proof.
   intros Pi F [A H].
   exists (fun a => negb (A a)).
+  unfold sat.
   now rewrite <- negvarEval.
 Qed.
 
